fd9a1ade95191292ef9eba324f30055a2e15e769
vitalii-semenov <paffy.don@gmail.com>
2019-11-04 16:31:30 +0200

    clear comments from js

---
+++ commentsTest.js
@@ -0,0 +1,34 @@
+    // Test Message
+let a = 5;
+let b = 10; /* asfasf
+dfsedf
+sdfsdf
+dsf */
+
+// Multi
+    // Line
+// Comment
+
+    // Test Message
+    let a = 5;
+    let b = 10; /* asfasf */
+    
+    // Multi
+        // Line
+    // Comment
+
+        // Test Message
+let a = 5;
+let b = 10; /* asfasf */
+
+// Multi
+    // Line
+// Comment
+
+    // Test Message
+    let a = 5;
+    let b = 10; /* asfasf */
+    
+    // Multi
+        // Line
+    // Comment
---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToFile = path.join(__dirname, 'commentsTest.js');
+
+const clearComments = (data) => {
+    const regEx = /(\/\/.*)|(\/\*[.\s\S]*?\*\/)/gm;
+    data.indexOf('//')
+    let res = data.replace(regEx, '');
+    fs.writeFile(pathToFile, res, err => {
+        if (err) throw new Error;
+        console.log('Comments successfully deleted')
+    });
+}
+
+fs.readFile(
+    pathToFile,
+    'utf-8',
+    (err, data) => {
+        if (err) throw new Error;
+        clearComments(data);
+    }
+)
